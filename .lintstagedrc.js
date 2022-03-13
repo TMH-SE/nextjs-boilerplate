e1e2f2b63aaa8646171ccd7e07fab149e0292cba
HieuM2VN <hieu.tm@mmh.co.jp>
2022-03-13 17:14:50 +0700

    feat(lint): update lint ruleset and update lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,22 @@
 const path = require('path')
 
+const buildPrettierCommand = (filenames) =>
+	`prettier --write ${filenames
+		.map((f) => path.relative(process.cwd(), f))
+		.join(' ')}`
+
 const buildEslintCommand = (filenames) =>
 	`next lint --fix --file ${filenames
 		.map((f) => path.relative(process.cwd(), f))
 		.join(' --file ')}`
 
+const buidlStylelintCommand = (filenames) =>
+	`stylelint ${filenames
+		.map((f) => path.relative(process.cwd(), f))
+		.join(' ')} --fix`
+
 module.exports = {
-	'*.{js,jsx,ts,tsx}': [buildEslintCommand],
-	'*.{css,scss,sass}': ['stylelint **/*.{css,scss,sass} -i .gitignore --fix'],
+	'*.{js,jsx,ts,tsx}': [buildPrettierCommand, buildEslintCommand],
+	'*.{css,scss,sass}': [buidlStylelintCommand],
+	'*.{html,json,md}': [buildPrettierCommand],
 }
